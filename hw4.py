fc5ab9ac47bbf5cead45d550918c11b8e6c4037f
enbattle <lis18@rpi.edu>
2019-12-05 11:24:13 -0500

    added hw4 files

---
+++ hw4.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+from concurrent import futures
+import sys  # For sys.argv, sys.exit()
+import socket  # for gethostbyname()
+
+import grpc
+
+import csci4220_hw4_pb2
+import csci4220_hw4_pb2_grpc
+
+def run():
+	if len(sys.argv) != 4:
+		print("Error, correct usage is {} [my id] [my port] [k]".format(sys.argv[0]))
+		sys.exit(-1)
+
+	local_id = int(sys.argv[1])
+	my_port = str(int(sys.argv[2])) # add_insecure_port() will want a string
+	k = int(sys.argv[3])
+	my_hostname = socket.gethostname() # Gets my host name
+	my_address = socket.gethostbyname(my_hostname) # Gets my IP address from my hostname
+
+	# creating the server socket
+	server = socket.socket()          
+	server.bind(('', my_port))         
+	server.listen(10)     
+
+	# a forever loop until we interrupt it or  
+	# an error occurs 
+	while True: 
+		# Establish connection with client. 
+		c, addr = server.accept()      
+		print('Got connection from ', addr)
+
+		# send a thank you message to the client.  
+		c.send('Thank you for connecting') 
+	  
+		# Close the connection with the client 
+		c.close() 
+
+	# creating the client socket
+	client = socket.socket()
+	client.connect((my_address, my_port))
+	print(client.recv(1024))
+	client.close()
+
+	''' Use the following code to convert a hostname to an IP and start a channel
+	Note that every stub needs a channel attached to it
+	When you are done with a channel you should call .close() on the channel.
+	Submitty may kill your program if you have too many file descriptors open
+	at the same time. '''
+	
+	#remote_addr = socket.gethostbyname(remote_addr_string)
+	#remote_port = int(remote_port_string)
+	#channel = grpc.insecure_channel(remote_addr + ':' + str(remote_port))
+
+if __name__ == '__main__':
+	run()
